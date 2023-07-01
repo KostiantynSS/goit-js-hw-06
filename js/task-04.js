01775622139e48db197bd1a8f316b739652908c1
KostiantynSS <storoghenko94@gmail.com>
2023-07-01 18:32:24 +0300

    hw4

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,3 @@
-// // <div id="counter">
-//   <button type="button" data-action="decrement">-1</button>
-//   <span id="value">0</span>
-//   <button type="button" data-action="increment">+1</button>
-// </div>
-
 const decrementBtn = document.querySelector("button[data-action='decrement']");
 const incrementBtn = document.querySelector("button[data-action='increment']");
 const totalValue = document.querySelector("#value");
---
+++ js/task-05.js
@@ -0,0 +1,2 @@
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
