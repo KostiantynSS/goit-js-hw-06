bc164aae54b41324475f77aefd2f1b8bbb472002
KostiantynSS <storoghenko94@gmail.com>
2023-07-01 19:23:31 +0300

    5task

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,10 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
+input.addEventListener("input", changeInput);
+
+function changeInput() {
+    if (input.value === "") {
+        return (output.textContent = "Anonymous");
+    }
+    output.textContent = input.value;
+}
