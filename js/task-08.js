f4faec95cd7aa7231c880c285b978c930c25e612
KostiantynSS <storoghenko94@gmail.com>
2023-07-02 18:41:02 +0300

    8task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", formSubmit);
 
+function formSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    const result = { email: email, password: password };
+
+    if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+    }
+
+    console.log(result);
+    event.currentTarget.reset();
+}
