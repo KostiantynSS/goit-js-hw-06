4d24faa4b272cb2fc4380457341713c3c754b99a
KostiantynSS <storoghenko94@gmail.com>
2023-07-02 19:32:46 +0300

    9task

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", changeColor);
+
+function changeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+    colorValue.textContent = getRandomHexColor();
+}
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
